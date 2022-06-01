f7a444bc66d0abd0dadb74f8852e116678e8ad2f
fatimakhatungit <fatimakhatuncse@gmail.com>
2022-06-01 19:12:20 +0600

    Search result page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,32 +4,98 @@ File:           JS Core
 Version:        1.0
 Last change:    00/00/00 
 -------------------------------------------------------------------------------- */
-(function ($) {
-    "use strict";
+// (function ($) {
+//     "use strict";
 
 
-    $('select').niceSelect();
+//     $('select').niceSelect();
 
 
-})(jQuery);
+// })(jQuery);
 
-//get input
-let input = document.getElementById("search");
-//get list of value
-let list = document.querySelectorAll(".list li");
+let suggestions = [ 
+    "Product support",
+  "What is AppSumo Plus?",
+  "My deal is live! Now what?",
+  "How do I ask for reviews once my product goes live?",  
+  "Product support", 
+  "AppSumo Affiliate Program",
+  "Update profile photo",
+  "Product Reviews and Questions",
+  "Manage email preferences or unsubscribe",
+  "How do I login to my licensed product?",
+  "How do I partner with AppSumo and submit my product?",
+  "How do I ask for reviews once my product goes live?", 
+  "Designer",
+  "Developer",
+  "Web Designer",
+];
 
-//function search on the list.
-function search (){
-  for(let i = 0; i < list.length; i += 1){
-   //check if the element contains the value of the input
-   if(list[i].innerText.toLowerCase().includes(input.value.toLowerCase())){
-     list[i].style.display = "block";
-   }else{
-     list[i].style.display = "none";
-   }
-  }
+
+
+//Create this file first before writing javascript
+
+
+
+
+
+// getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = "https://www.google.com/search?q=" + userData;
+            linkTag.setAttribute("href", webLink);
+            console.log(webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = '<li>'+ data +'</li>';
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
 }
 
-//to the change run search.
-input.addEventListener('input', search);
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = "https://www.google.com/search?q=" + selectData;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
 
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+ userValue +'</li>';
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData; 
+}
