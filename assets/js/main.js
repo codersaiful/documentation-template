a582f7f4897f7e28dee2f32056e833b558731080
fatimakhatungit <fatimakhatuncse@gmail.com>
2022-06-01 14:41:51 +0600

    search-results update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,23 @@ Last change:    00/00/00
 
 })(jQuery);
 
+//get input
+let input = document.getElementById("search");
+//get list of value
+let list = document.querySelectorAll(".list li");
+
+//function search on the list.
+function search (){
+  for(let i = 0; i < list.length; i += 1){
+   //check if the element contains the value of the input
+   if(list[i].innerText.toLowerCase().includes(input.value.toLowerCase())){
+     list[i].style.display = "block";
+   }else{
+     list[i].style.display = "none";
+   }
+  }
+}
+
+//to the change run search.
+input.addEventListener('input', search);
+
